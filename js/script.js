afcad18e8dad80ae7a8671e32dea7e26b389a0cf
Manuela Silva <silvafarias.manu07@gmail.com>
2024-03-13 15:35:31 -0300

    aula tres

--- js/script.js
+++ js/script.js
@@ -1,38 +1,66 @@
-let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
-let closeModal =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
-let modal = document.querySelector('#modal'); //Modal para edição das notas
-let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
-let notes = document.querySelector('#notes');//Lista divs com dados das notas
-let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
+const addNote = document.querySelector('#add-note');//Botão de para adicionar nota
+const closeModal =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
+const modal = document.querySelector('#modal'); //Modal para edição das notas
+const modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
+const notes = document.querySelector('#notes');//Lista divs com dados das notas
+const btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
+const btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
 
-addNote.addEventListener("click", (evt)=>{
+addNote.addEventListener("click", (evt) => {
     evt.preventDefault();
-   
-    notes.style.display = "none";
-    modal.style.display = "block";
-    addNote.style.display = "none";
-})
+    console.log("Botão abrindo!");
+    notes.style.display='none';
+    modal.style.display='block';
+    addNote.style.display='none';
+});
 
-
-btnCloseNote.addEventListener("click", (evt)=>{
+btnCloseNote.addEventListener("click", (evt) => {
     evt.preventDefault();
-    notes.style.display = "flex";
-    modal.style.display = "none";
-    addNote.style.display = "block";
-})
+    console.log("Botão fechando!");
+    notes.style.display="flex";
+    modal.style.display="none";
+    addNote.style.display='block';
+});
 
-
-btnSaveNote.addEventListener("click", (evt) =>{
+btnSaveNote.addEventListener("click", (evt) => {
     evt.preventDefault();
-    let data={ id: document.querySelector("#input-id").value,
-      title:document.querySelector("#input-title").value,
-      content:document.querySelector("#input-content").value,
-
-
+    let data = {id: document.querySelector("#input-id").value,
+                title:document.querySelector("#input-title").value,
+                content:document.querySelector("#input-content").value,
     }
-})
 
- 
+    saveNote(data);
+});
+
+
+const saveNote = (note) => {
+
+    let notes = loadNotes(); //let pq muda o valor, o const faz com que o valor fique sempre igual.
+    note.lastTime = new Date().getTime();
+
+    if(note.id.length>0){
+        //?
+    }else{
+        note.id = new Date().getTime();
+    }
+    notes.push(note);
+    
+    notes = JSON.stringify(notes);
+
+    localStorage.setItem('notes',notes);
+    console.log(notes);
+};
+
+const loadNotes = () =>{
+    let notes = localStorage.getItem('notes');
+
+    if(!notes){
+        notes=[];
+    }else{
+        notes = JSON.parse(notes);
+    }
+    return notes;
+};
+
 
